77a572dc57d128df1c44b229d219cff05052093d
LioneL Christopher Chetty <lionel@lionelchetty.dev>
2020-06-28 19:56:26 +0200

    GetClassificationOverrides

--- src/Investec.OpenBanking.RestClient/Interfaces/IClassificationService.cs
+++ src/Investec.OpenBanking.RestClient/Interfaces/IClassificationService.cs
@@ -1,9 +1,12 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using Investec.OpenBanking.RestClient.Services;
 
 namespace Investec.OpenBanking.RestClient.Interfaces
 {
     public interface IClassificationService
     {
         Task<string> LookupCategory(string wikiCode);
+        Task<List<ClassificationOverrideModel>> GetClassificationOverrides();
     }
 }
--- src/Investec.OpenBanking.RestClient/Services/ClassificationService.cs
+++ src/Investec.OpenBanking.RestClient/Services/ClassificationService.cs
@@ -1,18 +1,20 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using AngleSharp;
 using AngleSharp.Dom;
 using AngleSharp.Html.Dom;
 using Investec.OpenBanking.RestClient.Interfaces;
+using Newtonsoft.Json;
 
 namespace Investec.OpenBanking.RestClient.Services
 {
-    public class ClassificationService:IClassificationService
+    public class ClassificationService : IClassificationService
     {
-        private IBrowsingContext _browsingContext { get; set; }
-        private IConfiguration _browsingConfig { get; set; }
-
         public ClassificationService()
         {
             _browsingConfig = Configuration.Default
@@ -21,28 +23,83 @@ namespace Investec.OpenBanking.RestClient.Services
             _browsingContext = BrowsingContext.New(_browsingConfig);
         }
 
+        private IBrowsingContext _browsingContext { get; }
+        private IConfiguration _browsingConfig { get; }
+
+        private string ExecutingAppDirectoryPath
+        {
+            get
+            {
+                var codeBase = Assembly.GetExecutingAssembly().CodeBase;
+                var codeBaseUri = new UriBuilder(codeBase);
+                var uriPath = Uri.UnescapeDataString(codeBaseUri.Path);
+                return Path.GetDirectoryName(uriPath);
+            }
+        }
+
         public async Task<string> LookupCategory(string wikiCode)
         {
-            
-            var document = await _browsingContext.OpenAsync( $"https://en.wikipedia.org/wiki/{wikiCode}");
-            var vcard  = document.QuerySelector<IHtmlTableElement>(".infobox.vcard");
-            if (vcard != null)
+            try
             {
-                var industryRows = vcard.QuerySelectorAll<IHtmlTableCellElement>("th");
-                var industryRow = industryRows.Where(w => !string.IsNullOrEmpty(w.Text())).FirstOrDefault(f =>
-                    string.Equals(f.Text().Trim(), "industry",
-                        StringComparison.InvariantCultureIgnoreCase))?.NextElementSibling;
-                if (industryRow != null)
+                var document = await _browsingContext.OpenAsync($"https://en.wikipedia.org/wiki/{wikiCode}");
+                var vcard = document.QuerySelector<IHtmlTableElement>(".infobox.vcard");
+                if (vcard != null)
                 {
-                    var categories = industryRow.QuerySelectorAll<IHtmlAnchorElement>("a");
-                    if (categories != null && categories.Any())
+                    var industryRows = vcard.QuerySelectorAll<IHtmlTableCellElement>("th");
+                    var industryRow = industryRows.Where(w => !string.IsNullOrEmpty(w.Text())).FirstOrDefault(f =>
+                                                      string.Equals(f.Text().Trim(), "industry",
+                                                          StringComparison.InvariantCultureIgnoreCase))
+                                                  ?.NextElementSibling;
+                    if (industryRow != null)
                     {
-                        return categories.FirstOrDefault()?.Text().Trim();
+                        var categories = industryRow.QuerySelectorAll<IHtmlAnchorElement>("a");
+                        if (categories != null && categories.Any())
+                        {
+                            return categories.FirstOrDefault()?.Text().Trim();
+                        }
                     }
-                } 
+                }
             }
-            
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+            }
+
             return "";
         }
+
+        public async Task<List<ClassificationOverrideModel>> GetClassificationOverrides()
+        {
+            try
+            {
+                var fileInfo = new FileInfo($@"{ExecutingAppDirectoryPath}\classification-override.json");
+                if (fileInfo.Exists)
+                {
+                    var json = await File.ReadAllTextAsync(fileInfo.FullName);
+                    return JsonConvert.DeserializeObject<List<ClassificationOverrideModel>>(json);
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+            }
+
+            return new List<ClassificationOverrideModel>();
+        }
+    }
+
+    public class ClassificationOverrideModel
+    {
+        public enum OverrideType
+        {
+            Equals,
+            Contains
+        }
+
+        public string Lookup { get; set; }
+        public string Property { get; set; }
+        public string Value { get; set; }
+        public string Replace { get; set; }
+        public OverrideType Type { get; set; }
     }
 }
