78780cca98ab9b1e1829ba68bed44ce2b239f147
LioneL Christopher Chetty <dalion619@users.noreply.github.com>
2020-07-24 11:56:54 +0200

    Implemented date range filtering on account transactions API (#4)
    Co-authored-by: LioneL Christopher Chetty <lionel@lionelchetty.dev>

--- src/Investec.OpenBanking.RestClient/Interfaces/IInvestecOpenBankingClient.cs
+++ src/Investec.OpenBanking.RestClient/Interfaces/IInvestecOpenBankingClient.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using Investec.OpenBanking.RestClient.ResponseModels;
 using Investec.OpenBanking.RestClient.ResponseModels.Accounts;
@@ -8,7 +9,10 @@ namespace Investec.OpenBanking.RestClient.Interfaces
     {
         Task<AccessTokenResponseModel> GetAccessToken();
         Task<BaseResponseModel<AccountsResponseModel>> GetAccounts();
-        Task<BaseResponseModel<AccountTransactionsResponseModel>> GetAccountTransactions(string accountId);
+
+        Task<BaseResponseModel<AccountTransactionsResponseModel>> GetAccountTransactions(
+            string accountId, DateTime? fromDate = null, DateTime? toDate = null);
+
         Task<BaseResponseModel<AccountBalanceResponseModel>> GetAccountBalance(string accountId);
     }
 }
